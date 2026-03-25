7475fb41daf44f0262f498ce5bd4c7fda5704ea9
BurlyAxe <brunolarioshdz@gmail.com>
2026-03-24 21:35:45 -0600

    ligero cambio

--- src/controllers/videogameController.js
+++ src/controllers/videogameController.js
@@ -12,12 +12,13 @@ async function getVideogame(req, res) {
 
 async function getVideogameByName(req, res) {
     try {
-        const nameVideogame = await Videogame.find({ name: req.params.name});
-        res.status(200).json(nameVideogame);
+        const nameVideogame = await Videogame.findOne({ name: req.params.name });
 
-        if(!nameVideogame) {
-            return res.status(400).json({ message: "No hay resultados para esa busqueda"});
+        if (nameVideogame.length === 0) {
+            return res.status(400).json({ message: "No hay resultados para esa busqueda" });
         }
+
+        res.status(200).json(nameVideogame);
 
     } catch (error) {
         console.log(error)
@@ -26,12 +27,13 @@ async function getVideogameByName(req, res) {
 
 async function getVideogameByGenre(req, res) {
     try {
-        const genreVideogame = await Videogame.find({ genre: req.params.genre});
-        res.status(200).json(genreVideogame);
+        const genreVideogame = await Videogame.find({ genre: req.params.genre });
 
-        if(!genreVideogame) {
-            return res.status(400).json({ message: "No hay resultados para esa busqueda"});
+        if (genreVideogame.length === 0) {
+            return res.status(400).json({ message: "No hay resultados para esa busqueda" });
         }
+
+        res.status(200).json(genreVideogame);
     } catch (error) {
         console.log(error);
     };
@@ -41,17 +43,17 @@ async function createVideogame(req, res) {
     try {
         const { name, description, releaseDate, platforms, genre, classification, price } = req.body;
 
-        if(!name || !description || !releaseDate || !platforms || !Array.isArray(platforms) || !genre || !classification || !price){
-            return res.status(400).json({ message: "Todos los campos son requeridos"})
+        if (!name || !description || !releaseDate || !platforms || !Array.isArray(platforms) || !genre || !classification || !price) {
+            return res.status(400).json({ message: "Todos los campos son requeridos" })
         };
 
-        if(!mongoose.Types.ObjectId.isValid(genre) || !mongoose.Types.ObjectId.isValid(classification) || platforms.some(id => !mongoose.Types.ObjectId.isValid(id))) {
-            return res.status(400).json({ message: "Id no validos"})
+        if (!mongoose.Types.ObjectId.isValid(genre) || !mongoose.Types.ObjectId.isValid(classification) || platforms.some(id => !mongoose.Types.ObjectId.isValid(id))) {
+            return res.status(400).json({ message: "Id no validos" })
         };
 
         const newVideogame = await Videogame.create({
             name, description, releaseDate, platforms, genre, classification, price
-        }); 
+        });
 
         return res.status(200).json(newVideogame)
 
@@ -64,15 +66,15 @@ async function updateVideogame(req, res) {
     try {
         const { id } = req.params;
         const { name, price } = req.body;
-        
-        if(!name || !price || price < 1){
+
+        if (!name || !price || price < 1) {
             return res.status(400).json({ message: "Datos insuficientes" })
         };
 
-        const updateGame = await Videogame.findByIdAndUpdate(id, {name, price}, {new: true});
+        const updateGame = await Videogame.findByIdAndUpdate(id, { name, price }, { new: true });
 
-        if (updateGame){
-        return res.status(200).json(updateGame);
+        if (updateGame) {
+            return res.status(200).json(updateGame);
         } else {
             return res.status(400).json({ message: "Fallo al actualizar datos" })
         }
@@ -86,10 +88,10 @@ async function deleteVideogame(req, res) {
         const { id } = req.params;
         const deletedVideogame = await Videogame.findByIdAndDelete(id);
 
-        if(deletedVideogame) {
+        if (deletedVideogame) {
             return res.status(200).send()
         } else {
-            return res.status(400),json({ message: "Sin datos de videojuego" })
+            return res.status(400).json({ message: "Sin datos de videojuego" })
         };
 
     } catch (error) {
